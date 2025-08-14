6619705b416013ef8860e2a3e9596839882ccd62
Wayne Jenkins <wjenkins1@companieshouse.gov.uk>
2025-08-14 09:04:48 +0100

    Remove commented code

--- groups/advanced-search/module-advanced-search/security-groups.tf
+++ groups/advanced-search/module-advanced-search/security-groups.tf
@@ -3,13 +3,6 @@ resource "aws_security_group" "advanced_search_elasticsearch" {
   vpc_id      = var.vpc_id
 
   ingress = []
-#  ingress {
-#    from_port   = 443
-#    to_port     = 443
-#    protocol    = "tcp"
-#    description = "Inbound HTTPS access to Elasticsearch cluster endpoint from VPC"
-#    cidr_blocks = var.cluster_accessible_cidrs
-#  }
 
   tags = {
     Name         = "${var.environment}-${var.service}-elasticsearch-security-group"
